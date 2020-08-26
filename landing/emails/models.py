3d0efd2f2f1bfb010b9e5ea4ef8c11779b96dbbd
keys4words <keys4words@gmail.com>
2020-08-26 14:19:49 +0300

    add name and bio fields to emailentry model

--- landing/emails/models.py
+++ landing/emails/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class EmailEntry(models.Model):
     email = models.EmailField()
+    # date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    name = models.CharField(max_length=120, blank=True)
+    bio = models.TextField(blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # updated
+    # timestamp
