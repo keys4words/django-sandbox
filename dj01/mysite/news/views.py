3ea0064e1620183f8422f1819dc785824a47555a
keys4words <keys4words@gmail.com>
2021-04-08 17:11:26 +0300

    add model & migrate

--- dj01/mysite/news/views.py
+++ dj01/mysite/news/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    print(dir(request))
+    # print(dir(request))
     return HttpResponse('Simple response')
 
 
