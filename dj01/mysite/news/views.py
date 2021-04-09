bcefa68957b1ee2e12467b9c7ce1a938b45eedea
keys4words <keys4words@gmail.com>
2021-04-09 15:27:50 +0300

    add templates

--- dj01/mysite/news/views.py
+++ dj01/mysite/news/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 # Create your views here.
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Simple response')
+    news = News.objects.order_by('-created_at')
+    # res = '<h1>List of News</h1>'
+    # for el in news:
+    #     res += f'<div><p>{el.title}</p><p>{el.content}</p></div><hr>'
+    # return HttpResponse(res)
+    context = {
+        'news': news,
+        'title': 'News Page'
+    }
+    return render(request, 'news/index.html', context)
 
 
 def test(request):
