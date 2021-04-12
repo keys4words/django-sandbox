b93809334d6b66db672808e962f01ae45c2134ed
keys4words <keys4words@gmail.com>
2021-04-12 16:04:35 +0300

    add category to model

--- dj01/mysite/news/models.py
+++ dj01/mysite/news/models.py
@@ -9,6 +9,8 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Active')
 
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
     def __str__(self):
         return f'<{self.title}>'
 
@@ -17,3 +19,15 @@ class News(models.Model):
         verbose_name = '\'News\''
         verbose_name_plural = 'News'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Category name')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
