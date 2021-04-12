0032ab55d0c164958c39e6d7376a8857cf88cb55
keys4words <keys4words@gmail.com>
2021-04-12 11:34:15 +0300

    fix model

--- dj01/mysite/news/models.py
+++ dj01/mysite/news/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='News Title')
+    content = models.TextField(blank=True, verbose_name='News content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of creation')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Date of last edit')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Active')
 
     def __str__(self):
         return f'<{self.title}>'
+
+
+    class Meta:
+        verbose_name = '\'News\''
+        verbose_name_plural = 'News'
+        ordering = ['-created_at']
--- dj01/mysite/news/views.py
+++ dj01/mysite/news/views.py
@@ -6,11 +6,7 @@ from .models import News
 
 # Create your views here.
 def index(request):
-    news = News.objects.order_by('-created_at')
-    # res = '<h1>List of News</h1>'
-    # for el in news:
-    #     res += f'<div><p>{el.title}</p><p>{el.content}</p></div><hr>'
-    # return HttpResponse(res)
+    news = News.objects.all()
     context = {
         'news': news,
         'title': 'News Page'
